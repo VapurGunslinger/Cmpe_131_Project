a4b42d86f22bd92ddae9eadf1e33ffae63a8dbb0
VapurGunslinger <144088410+VapurGunslinger@users.noreply.github.com>
2024-03-16 20:12:51 -0700

    Working julio (#3)
    * this
    
    * modified:   Flask server back-end/views.py
    	modified:   Flask server back-end/views.py
    
    * modified:   Flask server back-end/views.py
    	new file:   templates/Home.html
    	new file:   templates/base.html
    
    * modified:   Flask server back-end/app.py
    	new file:   static/main.css
    	modified:   templates/base.html
    
    * modified:   Flask server back-end/app.py
    
    * modified:   .gitignore
    	new file:   .venv/Include/site/python3.12/greenlet/greenlet.h
    	new file:   .venv/Scripts/Activate.ps1
    	new file:   .venv/Scripts/activate
    	new file:   .venv/Scripts/activate.bat
    	new file:   .venv/Scripts/deactivate.bat
    	new file:   .venv/Scripts/flask.exe
    	new file:   .venv/Scripts/pip.exe
    	new file:   .venv/Scripts/pip3.12.exe
    	new file:   .venv/Scripts/pip3.exe
    	new file:   .venv/Scripts/python.exe
    	new file:   .venv/Scripts/pythonw.exe
    	new file:   .venv/pyvenv.cfg
    
    * modified:   Flask server back-end/app.py
    	renamed:    static/main.css -> Flask server back-end/static/main.css
    	new file:   Flask server back-end/templates/adoption_steps.html
    	renamed:    templates/Home.html -> Flask server back-end/templates/animals.html
    	renamed:    templates/base.html -> Flask server back-end/templates/base.html
    	new file:   Flask server back-end/templates/home.html
    	new file:   Flask server back-end/templates/testimonials.html
    	modified:   Flask server back-end/views.py
    
    * modified:   Flask server back-end/app.py
    	deleted:    Flask server back-end/static/main.css
    	new file:   Flask server back-end/static/styles/main.css
    	modified:   Flask server back-end/templates/adoption_steps.html
    	modified:   Flask server back-end/templates/animals.html
    	modified:   Flask server back-end/templates/base.html
    	modified:   Flask server back-end/templates/home.html
    	modified:   Flask server back-end/templates/testimonials.html
    
    * modified:   Flask server back-end/app.py
    	modified:   Flask server back-end/static/styles/main.css
    	modified:   Flask server back-end/templates/adoption_steps.html
    	modified:   Flask server back-end/templates/animals.html
    	modified:   Flask server back-end/templates/base.html
    	modified:   Flask server back-end/templates/home.html
    	modified:   Flask server back-end/templates/testimonials.html
    
    * modified:   Flask server back-end/app.py
    	modified:   Flask server back-end/views.py
    
    ---------
    
    Co-authored-by: VapurGunslinger <jcoronado-toscano21@gmail.com>

--- Flask server back-end/app.py
+++ Flask server back-end/app.py
@@ -6,29 +6,35 @@ from views import views
 
 app = Flask(__name__)
 app.config['SQLACHEMY_DATABASE_URI']= 'sqlite:///test.db'
+#db= SQLAlchemy(app)
 
-db= SQLAlchemy(app)
-
-class Appointment(db.modle):
-    name= db.Colomn (db.String(100),nullable=False)
-    phoneNumber =db.Colomn(db.String(10), nullable=False)
+# class Appointment(db.modle):
+#     name= db.Colomn (db.String(100),nullable=False)
+#     phoneNumber =db.Colomn(db.String(10), nullable=False)
     
-    #date_created= db.Colomn(db.DateTime, default datetime.utcnow)
-    def __repr__(self):
-        return '<Appointment %r>' % self.name #%self.appointment_date
+#     #date_created= db.Colomn(db.DateTime, default datetime.utcnow)
+#     def __repr__(self):
+#         return '<Appointment %r>' % self.name #%self.appointment_date
 
-#employ side claander fill here.
+##employ side claander fill here.
 
-app.register_blueprint(views, url_prefix=("/"))
+##app.register_blueprint(views, url_prefix=("/"))
 
-@app.route('/', methods= ['POST','GET'])
-def index():
-    if request.method =='POST':
-        pass
-        #return some page
-    else:
-        return render_template('home.html')
+@app.route('/')
+def home():
+    return render_template('home.html')
 
+@app.route('/Adoption_steps')
+def adoption_steps():
+    return render_template('Adoption_steps.html')
+
+@app.route('/Animals')
+def animals():
+    return render_template('Animals.html')
+
+@app.route('/Testimonial')
+def testimonial():
+    return render_template('Testimonials.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- Flask server back-end/views.py
+++ Flask server back-end/views.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
+from flask import Flask, render_template, url_for , request
+from flask_sqlalchemy import SQLAlchemy
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@views.route('/')
 def home():
-    return "home page"
+
+    return render_template('home.html')
+
 
 def test():
     return 0
