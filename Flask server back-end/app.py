2a6cbb8a0c5ec6d5cf3c5b92b90c7d0f7986c559
kcoyo4 <kyoca9@gmail.com>
2024-04-04 18:45:26 -0700

    removed .venv, added requirements.txt

--- Flask server back-end/app.py
+++ Flask server back-end/app.py
@@ -1,28 +1,33 @@
-from flask import Flask, render_template, url_for , request
-# from flask_sqlalchemy import SQLAlchemy
-#from datatime import datatime
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 from views import views
 
 app = Flask(__name__, static_folder="static")
-app.config['SQLACHEMY_DATABASE_URI']= 'sqlite:///test.db'
-#db= SQLAlchemy(app)
+app.config['SQLACHEMY_DATABASE_URI']= 'sqlite:///animals.db'
+# db= SQLAlchemy(app)
 
-# class Appointment(db.modle):
-#     name= db.Colomn (db.String(100),nullable=False)
+
+
+
+
+
+
+# class Appointment(db.module):
+#     name= db.Column (db.String(100),nullable=False)
 #     phoneNumber =db.Colomn(db.String(10), nullable=False)
     
 #     #date_created= db.Colomn(db.DateTime, default datetime.utcnow)
 #     def __repr__(self):
 #         return '<Appointment %r>' % self.name #%self.appointment_date
 
-##employ side claander fill here.
 
 ##app.register_blueprint(views, url_prefix=("/"))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/Adoption_steps')
 def adoption_steps():
